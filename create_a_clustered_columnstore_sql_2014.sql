be879abca4f854a67cfee9dc8086be64e070c649
Oliver <37679945+marmightygood@users.noreply.github.com>
2019-01-28 16:06:21 +1300

    Update create_a_clustered_columnstore_sql_2014.sql
    Add comments

--- create_a_clustered_columnstore_sql_2014.sql
+++ create_a_clustered_columnstore_sql_2014.sql
@@ -16,21 +16,25 @@ DECLARE @SCHEMA_NAME SYSNAME = '$(schema_name)';
 DECLARE @NAME SYSNAME = '$(table_name)';
 DECLARE @Code NVARCHAR(MAX) = ''
 
+--Generate code that drops unique keys etc
 SELECT @Code += '
 ' + COALESCE('ALTER TABLE ' + OBJECT_SCHEMA_NAME(parent_object_id) + '.' + OBJECT_NAME(parent_object_id) + ' DROP CONSTRAINT ' + name, '') FROM sys.key_constraints WHERE OBJECT_NAME(parent_object_id) = @NAME;
 
+--Generate code that drops foreign keys 
 SELECT @Code += '
 ' + COALESCE('ALTER TABLE ' + OBJECT_SCHEMA_NAME(parent_object_id) + '.' + OBJECT_NAME(parent_object_id) + ' DROP CONSTRAINT ' + name, '') FROM sys.foreign_keys WHERE OBJECT_NAME(parent_object_id) = @NAME;
 
+--Generate code that drops indexes
 SELECT @Code += '
 ' + COALESCE('DROP INDEX ' + name + ' ON ' + OBJECT_SCHEMA_NAME(object_id) + '.' + OBJECT_NAME(object_id), '')  FROM sys.indexes WHERE OBJECT_NAME(object_id) = @NAME;
 
 /*
-If the columnstore needs to be partitioned, this line will be required:
+If the columnstore needs to be partitioned, this line will be required to storage align the table:
 SELECT @Code += '
 ' + COALESCE('CREATE CLUSTERED INDEX CI_' + @SCHEMA_NAME + '_' + @NAME + ' ON ' + @SCHEMA_NAME + '.' + @NAME + '($(partition_columns)) ON $(partition_scheme) ($(partition_columns));', '')
 */
 
+--Generate code to create a columnstore
 SELECT @Code += '
 ' + COALESCE('CREATE CLUSTERED COLUMNSTORE INDEX CI_' + @SCHEMA_NAME + '_' + @NAME + ' ON ' + @SCHEMA_NAME + '.' + @NAME + ' WITH (DROP_Existing=ON) --ON $(partition_scheme)($(partition_columns)) ', '')
 
