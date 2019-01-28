f821f260c74d6fcf08323903fa9c57b620a0ca04
Oliver <37679945+marmightygood@users.noreply.github.com>
2019-01-29 09:15:47 +1300

    Update create_a_clustered_columnstore_sql_2014.sql

--- create_a_clustered_columnstore_sql_2014.sql
+++ create_a_clustered_columnstore_sql_2014.sql
@@ -12,10 +12,10 @@ useless for any version of SQL other than
 
 
 
-DECLARE @SCHEMA_NAME SYSNAME = '$(<schema_name,varchar,dbo>)';
-DECLARE @NAME SYSNAME = '$(<table_name,varchar,my_table>)';
-DECLARE @PartitionScheme = '$(<partition_scheme,varchar,PS_PartitionScheme>)'
-DECLARE @PartitioningColumns =  '$(<partition_columns,varchar,Id>)'
+DECLARE @SCHEMA_NAME SYSNAME = '<schema_name,varchar,dbo>';
+DECLARE @NAME SYSNAME = '<table_name,varchar,my_table>)';
+DECLARE @PartitionScheme = '<partition_scheme,varchar,PS_PartitionScheme>'
+DECLARE @PartitioningColumns =  '<partition_columns,varchar,Id>'
 DECLARE @Code NVARCHAR(MAX) = ''
 
 SELECT @Code += '
@@ -27,13 +27,13 @@ SELECT @Code += '
 SELECT @Code += '
 ' + COALESCE('DROP INDEX ' + name + ' ON ' + OBJECT_SCHEMA_NAME(object_id) + '.' + OBJECT_NAME(object_id), '')  FROM sys.indexes WHERE OBJECT_NAME(object_id) = @NAME;
 
-/*
-If the columnstore needs to be partitioned, this line will be required:
+
+--If the columnstore needs to be partitioned, this line will be required:
 SELECT @Code += '
-' + COALESCE('CREATE CLUSTERED INDEX CI_' + @SCHEMA_NAME + '_' + @NAME + ' ON ' + @SCHEMA_NAME + '.' + @NAME + '(TradingDateKey) ON $('+@PartitionScheme+')($('+@PartitioningColumns+'));', '')
-*/
+' + COALESCE('CREATE CLUSTERED INDEX CI_' + @SCHEMA_NAME + '_' + @NAME + ' ON ' + @SCHEMA_NAME + '.' + @NAME + '(TradingDateKey) ON('+@PartitionScheme+'('+@PartitioningColumns+');', '')
+
 
 SELECT @Code += '
-' + COALESCE('CREATE CLUSTERED COLUMNSTORE INDEX CI_' + @SCHEMA_NAME + '_' + @NAME + ' ON ' + @SCHEMA_NAME + '.' + @NAME + ' WITH (DROP_Existing=ON) --ON ' + @PartitionScheme + '(' + @PartitioningColumns + ')', '')
+' + COALESCE('CREATE CLUSTERED COLUMNSTORE INDEX CI_' + @SCHEMA_NAME + '_' + @NAME + ' ON ' + @SCHEMA_NAME + '.' + @NAME + ' WITH (DROP_Existing=ON) ON ' + @PartitionScheme + '(' + @PartitioningColumns + ')', '')
 
 PRINT @Code
