31b77eb5e8eaadabe80535fa77f19ef56dcca2ff
Oliver <37679945+marmightygood@users.noreply.github.com>
2019-02-19 08:55:46 +1300

    Update get_size_of_tables_partitions.sql

--- get_size_of_tables_partitions.sql
+++ get_size_of_tables_partitions.sql
@@ -10,9 +10,7 @@ INNER JOIN sys.partitions p ON i.object_id = p.OBJECT_ID
 	AND i.index_id = p.index_id
 LEFT JOIN sys.schemas s ON t.schema_id = s.schema_id
 LEFT JOIN sys.partition_schemes ps ON i.data_space_id = ps.data_space_id
-WHERE t.NAME NOT LIKE 'dt%'
-	AND t.is_ms_shipped = 0
+WHERE   t.is_ms_shipped = 0
 	AND i.OBJECT_ID > 255
-	AND ps.Name IS NULL
 ORDER BY s.name,
 	t.name
