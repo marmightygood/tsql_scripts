353375a866bb2d174db0edcb83c5547806f35746
Oliver <37679945+marmightygood@users.noreply.github.com>
2019-02-19 09:02:44 +1300

    Update get_size_of_tables_partitions.sql

--- get_size_of_tables_partitions.sql
+++ get_size_of_tables_partitions.sql
@@ -2,21 +2,26 @@
 SELECT 
 	s.name SchemaName,
 	t.name TableName,
+	i.name IndexName,
 	p.partition_number PartitionNumber,
 	p.rows PartitionRowCount,
-	ps.name PartitionFunctionName
+	ps.name PartitionSchemeName
 FROM 
 	sys.tables t
-	INNER JOIN sys.indexes i ON t.OBJECT_ID = i.object_id
-	INNER JOIN sys.partitions p ON i.object_id = p.OBJECT_ID
+	LEFT JOIN sys.indexes i 
+		ON t.OBJECT_ID = i.object_id
+		--AND i.name =   'pk_'+ s.name+ '_' + t.name
+	INNER JOIN sys.partitions p 
+		ON i.object_id = p.OBJECT_ID
 		AND i.index_id = p.index_id
-	LEFT JOIN sys.schemas s ON t.schema_id = s.schema_id
-	LEFT JOIN sys.partition_schemes ps ON i.data_space_id = ps.data_space_id
+	LEFT JOIN sys.schemas s 
+		ON t.schema_id = s.schema_id
+	LEFT JOIN sys.partition_schemes ps 
+		ON i.data_space_id = ps.data_space_id
 WHERE 
 	t.NAME NOT LIKE 'dt%'
 	AND t.is_ms_shipped = 0
 	AND i.OBJECT_ID > 255
-	AND s.Name = 'Stage'
 ORDER BY 
 	s.name,
 	t.name
