500f5f94fa4d9451d6172e0f6bb054ebf3e2f3d0
Oliver <37679945+marmightygood@users.noreply.github.com>
2019-02-19 16:08:57 +1300

    Update re-run_a_script_until_it_succeeds.sql

--- re-run_a_script_until_it_succeeds.sql
+++ re-run_a_script_until_it_succeeds.sql
@@ -5,17 +5,17 @@ DECLARE @Attempt INT = 1;
 WHILE @Success = 0 BEGIN
 
 	BEGIN TRY
-		
-    --Script to re-run goes here
-    
-    SET @Success = 1;
+
+	    --Script to re-run goes here
+
+	    SET @Success = 1;
 	END TRY
 	BEGIN CATCH
-    DECLARE @Message NVARCHAR(MAX);
-    SELECT @Message = CONVERT(VARCHAR(30), GETDATE(), 120) +  '' + ' - attempt ' + CAST(@Attempt as NVARCHAR(10));
-		RAISERROR (@Message, 1, 1)
-		SET @Attempt += 1;
-		SET @Success = 0;
+	    DECLARE @Message NVARCHAR(MAX);
+	    SELECT @Message = CONVERT(VARCHAR(30), GETDATE(), 120) +  '' + ' - attempt ' + CAST(@Attempt as NVARCHAR(10));
+			RAISERROR (@Message, 1, 1)
+			SET @Attempt += 1;
+			SET @Success = 0;
 	END CATCH
 
 END
